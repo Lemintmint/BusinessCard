654ee159153ca74dcbd433b6b496fbb60a09ed01
Lemintmint <lemgm1237@gmail.com>
2024-05-06 00:40:22 +0900

    2024/05/05 14:44

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.businesscard
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Person
@@ -23,10 +24,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
+
+//import kotlinx.coroutines.flow.internal.NoOpContinuation.context
+//import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,10 +84,9 @@ fun NameCard(): Unit {
 
 @Composable
 fun AddressCard(): Unit {
-    val MyAppIcons = Icons.Rounded
+    val myAppIcons = Icons.Rounded
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
         modifier = Modifier
     ) {
         Row(
@@ -87,10 +94,31 @@ fun AddressCard(): Unit {
                 .weight(1f)
         ) {
             Icon(
-                MyAppIcons.Person,
+                myAppIcons.Person,
                 contentDescription = null
             )
 
+            Text(
+                text = buildAnnotatedString {
+                    append(stringResource(id = R.string.twitter))
+                    addStyle(
+                        style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                            color = colorResource(id = R.color.purple_500),
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        start = 0,
+                        end = this.length
+                    )
+                },
+                modifier = Modifier
+                    .clickable(
+                        onClick = {
+                            val intent = Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://twitter.com/le_M_int"))
+                            context.startActivity(intent)
+                        }
+                    )
+            )
         }
     }
 }
