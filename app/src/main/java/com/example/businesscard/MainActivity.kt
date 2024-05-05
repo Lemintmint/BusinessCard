38d2949f5de19c511e86fcaec0577e94087bd001
Lemintmint <lemgm1237@gmail.com>
2024-05-05 14:44:52 +0900

    2024/05/05 14:44

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NameCard()
+                    AddressCard()
                 }
             }
         }
@@ -30,17 +39,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NameCard():Unit {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.leminticon),
+            contentDescription = "Lemint's Icon",
+            modifier = Modifier
+        )
+        text(
+            text = stringResource(id = R.string.name),
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        text(
+            text = stringResource(id = R.string.title),
+            fontSize = 12.sp,
+            modifier = Modifier
+        )
+    }
+}
+
+
+@Composable
+fun AddressCard():Unit {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CardPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        NameCard()
+        AddressCard()
     }
 }
