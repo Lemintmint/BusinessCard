6ca44bcd4a5adb70df92a5c02e6c602a2e2cc7e1
Lemintmint <lemgm1237@gmail.com>
2024-05-09 00:16:31 +0900

    2024/05/05 14:44

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,16 +5,19 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -30,14 +33,14 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import com.example.businesscard.ui.theme.BusinessCardTheme
+
 //import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 //import kotlinx.coroutines.flow.internal.NoOpContinuation.context
 //import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -61,7 +64,7 @@ fun NameCard(): Unit {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
-            //.background(color = colorResource(id = R.color.purple_200))
+            .background(color = colorResource(id = R.color.purple_200))
     ) {
         Image(
             painter = painterResource(id = R.drawable.leminticon),
@@ -86,42 +89,158 @@ fun NameCard(): Unit {
 @Composable
 fun AddressCard(): Unit {
     val myAppIcons = Icons.Rounded
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
+            .padding(bottom = 32.dp)
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .weight(1f)
+                .background(color = colorResource(id = R.color.teal_200))
+                .padding(start = 100.dp, end = 100.dp, top = 16.dp, bottom = 16.dp)
         ) {
-            Icon(
-                myAppIcons.Person,
-                contentDescription = null
-            )
-
-            Text(
-                text = buildAnnotatedString {
-                    append(stringResource(id = R.string.twitter))
-                    addStyle(
-                        style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
-                            color = colorResource(id = R.color.purple_500),
-                            textDecoration = TextDecoration.Underline
-                        ),
-                        start = 0,
-                        end = this.length
-                    )
-                },
+            Row(
                 modifier = Modifier
-                    .clickable(
-                        onClick = {
-                            val intent = Intent(Intent.ACTION_VIEW,
-                                Uri.parse("https://twitter.com/le_M_int"))
-                            //startActivity(this@MainActivity, intent, null)
-                        }
-                    )
-            )
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    myAppIcons.Person,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+
+                Text(
+                    text = buildAnnotatedString {
+                        append(stringResource(id = R.string.twitter))
+                        addStyle(
+                            style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                                color = colorResource(id = R.color.purple_500),
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 24.sp
+                            ),
+                            start = 0,
+                            end = this.length
+                        )
+                    },
+                    modifier = Modifier
+                        .clickable(
+                            onClick = {
+                                val intent = Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("https://twitter.com/le_M_int")
+                                )
+                                //startActivity(this@MainActivity, intent, null)
+                            }
+                        )
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    myAppIcons.PlayArrow,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+
+                Text(
+                    text = buildAnnotatedString {
+                        append(stringResource(id = R.string.niconico))
+                        addStyle(
+                            style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                                color = colorResource(id = R.color.purple_500),
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 24.sp
+                            ),
+                            start = 0,
+                            end = this.length
+                        )
+                    },
+                    modifier = Modifier
+                        .clickable(
+                            onClick = {
+                                val intent = Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("https://www.nicovideo.jp/user/121636330")
+                                )
+                                //startActivity(this@MainActivity, intent, null)
+                            }
+                        )
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    myAppIcons.PlayArrow,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+
+                Text(
+                    text = buildAnnotatedString {
+                        append(stringResource(id = R.string.youtube))
+                        addStyle(
+                            style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                                color = colorResource(id = R.color.purple_500),
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 24.sp
+                            ),
+                            start = 0,
+                            end = this.length
+                        )
+                    },
+                    modifier = Modifier
+                        .clickable(
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://www.youtube.com/channel/UCHJbC0yAotni47fFwrexqdQ"))
+                                //startActivity(this@MainActivity, intent, null)
+                            }
+                        )
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    myAppIcons.PlayArrow,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+
+                Text(
+                    text = buildAnnotatedString {
+                        append(stringResource(id = R.string.piapro))
+                        addStyle(
+                            style = MaterialTheme.typography.bodyLarge.toSpanStyle().copy(
+                                color = colorResource(id = R.color.purple_500),
+                                textDecoration = TextDecoration.Underline,
+                                fontSize = 24.sp
+                            ),
+                            start = 0,
+                            end = this.length
+                        )
+                    },
+                    modifier = Modifier
+                        .clickable(
+                            onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://piapro.jp/Lemintp"))
+                                //startActivity(this@MainActivity, intent, null)
+                            }
+                        )
+                )
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
